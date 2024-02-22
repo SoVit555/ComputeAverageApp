3a3bb4a7871f7ea96c02714f04c81255ef1fdcdb
beltran.061035 <beltran.061035@LAB-A-PC26>
2024-02-23 07:45:56 +0800

    Add project files.

---
+++ ComputeAverageProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputeAverageApp
+{
+    internal class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            double average = 0;
+            Console.WriteLine("Enter 5 gradee separated by new line:");
+            for (int i = 0; i < 5; i++) 
+            {
+                average += Convert.ToDouble(Console.ReadLine());
+            }
+            average /= 5;
+            Console.WriteLine("The average is " + average + " and round off to "+ Math.Round(average));
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
